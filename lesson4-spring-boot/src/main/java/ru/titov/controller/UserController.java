6a9d6a9eb62d29ebc24d50f50e90784597e8a08f
Kashevar128 <steve128@yandex.ru>
2022-08-24 17:02:51 +0300

    доп. материалы 4-го уока

--- lesson4-spring-boot/src/main/java/ru/titov/controller/UserController.java
+++ lesson4-spring-boot/src/main/java/ru/titov/controller/UserController.java
@@ -2,26 +2,23 @@ package ru.titov.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.titov.exeptions.EntityNotFoundException;
 import ru.titov.persist.User;
 import ru.titov.persist.UserRepository;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Slf4j
 @Controller
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
-
-
-
 
     private final UserRepository userRepository;
 
@@ -33,7 +30,14 @@ public class UserController {
 
     @GetMapping("/{id}")
     public String form(@PathVariable("id") long id, Model model) {
-        model.addAttribute("user", userRepository.findById(id));
+//        Optional<User> optionalUser = userRepository.findById(id);
+//        if (optionalUser.isPresent()) {
+//            model.addAttribute("user",  optionalUser);
+//        } else {
+//            throw new EntityNotFoundException(String.format("User with id %s not found", id));
+//        }
+        model.addAttribute("user", userRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException(String.format("User with id %s not found", id))));
         return "user_form";
     }
 
@@ -43,14 +47,14 @@ public class UserController {
         return "user_form";
     }
 
-    @GetMapping("/delete/{id}")
+    @DeleteMapping("{id}")
     public String deleteUserById(@PathVariable long id) {
         userRepository.delete(id);
         return "redirect:/user";
     }
 
     @PostMapping
-    public String saveUser(@Valid User user, BindingResult bindingResult) {
+    public String saveUser(@RequestBody @Valid User user, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "user_form";
         }
@@ -66,6 +70,13 @@ public class UserController {
     public String updateUser(User user) {
         userRepository.save(user);
         return "redirect:/user";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFoundExceptionHandler(Model model, EntityNotFoundException e) {
+        model.addAttribute("message", e.getMessage());
+        return "not_found";
     }
 
 
